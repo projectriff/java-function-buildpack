7def349be8d7326809f1b17ee7096c7f7c8c07e4
Scott Andrews <sandrews@pivotal.io>
2020-03-04 11:20:03 -0500

    Use go.mod to control pack version (#162)
    This will allow dependabot to managed the pack version for us, and also
    decouples the build from the version of pack that happens to be
    available on the workstation's path.

--- ci/tools.go
+++ ci/tools.go
@@ -3,4 +3,7 @@
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
-import _ "github.com/cloudfoundry/build-system-cnb/buildsystem"
+import (
+	_ "github.com/buildpacks/pack"
+	_ "github.com/cloudfoundry/build-system-cnb/buildsystem"
+)
